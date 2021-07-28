71011b6d22dabc9c98da41f3c6817104f88574db
mahme <mustafasaleh.hf@gmail.com>
2021-07-28 18:17:40 +0200

    router basics

--- src/index.js
+++ src/index.js
@@ -1,22 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter, Route } from 'react-router-dom'
-
-function Contact() {
-  return <h1>Contact Page</h1>
-}
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
 
 function Home() {
-  return <h1>Home Page</h1>
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
 }
 
-const App = () => (
-  <BrowserRouter>
+function Users() {
+  const users = [
+    {id: 1, name: 'ahmed'},
+    {id: 2, name: 'ali'},
+  ]
+
+  return (
     <div>
-      <Route path="/" component={Home} />
-      <Route path="/contact" component={Contact} />
+      <h2>Users</h2>
+      <hr />
+      {users.map(user => (
+        <div key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.name}</Link>
+        </div>
+      ))}
     </div>
-  </BrowserRouter>
-)
+  )
+}
+
+function UserProfile(props) {
+  console.log(props)
+  return (
+    <div>
+      <h1>UserProfile: {props.match.params.userId}</h1>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Link to="/">Home</Link> <Link to="/users">Users</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/users" exact component={Users} />
+          <Route path="/users/:userId" component={UserProfile} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
